d127568d902d4078fdee540fde21eba8846c7774
hirano <rhi691508@gmail.com>
2023-02-22 10:47:06 +0900

    Review of useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [status, setStatus] = useState<string | number>("text");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h4>{status}</h4>
+        <button onClick={() => setStatus(1)}>Button</button>
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -53,6 +55,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
